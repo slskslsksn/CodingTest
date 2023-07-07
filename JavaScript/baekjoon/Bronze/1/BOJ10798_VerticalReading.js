9553cbd93359a80bf733eed103a14f1a1f90d956
slskslsksn <slskslsksn@naver.com>
2023-07-07 18:33:43 +0900

    Try to fix the code BOJ 10798 Vertical Reading

--- JavaScript/baekjoon/Bronze/1/BOJ10798_VerticalReading.js
+++ JavaScript/baekjoon/Bronze/1/BOJ10798_VerticalReading.js
@@ -1,14 +1,25 @@
-const input = require('fs').readFileSync('BOJ10798_VerticalReading.txt').toString().trim().split('\n');
-const arr = [];
+const input = require('fs').readFileSync('BOJ10798_VerticalReading.txt').toString().trim().split('\n').map(e=>e.trim().split(''));
 let line = '';
-let max = 0;
-for (let i = 0; i < input.length; i++) {
-  max = Math.max(max, input[i].trim().length);
-  arr.push(input[i].trim().split(''));
-}
-for(let i=0; i<max; i++){
-  for(let j=0; j<5; j++){
-    if(arr[j][i])line+=arr[j][i];
+while(true){
+  let x = 0;
+  for(let i =0; i<5; i++){
+    const c = input[i].shift();
+    if(!c) x++;
+    else line += c;
   }
+  if(x===5) break;
 }
 console.log(line);
+// const arr = [];
+// let line = '';
+// let max = 0;
+// for (let i = 0; i < input.length; i++) {
+//   max = Math.max(max, input[i].trim().length);
+//   arr.push(input[i].trim().split(''));
+// }
+// for(let i=0; i<max; i++){
+//   for(let j=0; j<5; j++){
+//     if(arr[j][i])line+=arr[j][i];
+//   }
+// }
+// console.log(line);
