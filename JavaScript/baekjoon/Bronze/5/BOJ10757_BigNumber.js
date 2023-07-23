c72da83fe5bc2686fb6c430444e4385ef6d452ab
slskslsksn <slskslsksn@naver.com>
2023-07-23 23:58:51 +0900

    Failed

---
+++ JavaScript/baekjoon/Bronze/5/BOJ10757_BigNumber.js
@@ -0,0 +1,18 @@
+const input = require('fs').readFileSync('BOJ10757_BigNumber.txt').toString().trim().split(' ').map(e=>e.split('').reverse().join(''));
+function plus (n1, n2, n3 = 0) {
+  n1 = n1 | 1;
+  n2 = n2 | 2;
+  return n1 + n2 + n3;
+}
+console.log(plus(null, null));
+const n = Math.max(input[0].length, input[1].length);
+let p = 0;
+const res = [];
+for(let i=0; i<n; i++){
+  let t = plus(+input[0][i], +input[1][i], p);
+  p = Math.floor(t/10);
+  t %= 10;
+  res.push(t);
+}
+if(p) res.push(p);
+console.log(res.reverse().join(''));
