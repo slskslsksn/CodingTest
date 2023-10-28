4694e732d8b0c98e8931f34d29860c7a6c785ac8
slskslsksn <slskslsksn@naver.com>
2023-10-28 16:16:51 +0900

    Baekjoon 14581 Hongjun surrounded by fans B5

---
+++ JavaScript/baekjoon/Bronze/5/BOJ14581_HongjunSurroundedByFans.js
@@ -0,0 +1,5 @@
+// const id = require('fs').readFileSync('/dev/stdin','utf-8').trim();
+const id = 'honjun';
+console.log(':fan::fan::fan:');
+console.log(`:fan::${id}::fan:`);
+console.log(':fan::fan::fan:');
